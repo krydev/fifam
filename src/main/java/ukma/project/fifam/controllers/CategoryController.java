ea8d4a5fa7cc31aa9704f4d23fd2e1fb79f131d8
krydev <kryitdev@gmail.com>
2020-02-20 22:25:01 +0200

    Add categories controller

---
+++ src/main/java/ukma/project/fifam/controllers/CategoryController.java
@@ -0,0 +1,109 @@
+package ukma.project.fifam.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ukma.project.fifam.Frequency;
+import ukma.project.fifam.models.Category;
+import ukma.project.fifam.models.User;
+import ukma.project.fifam.repos.CategoryRepo;
+import ukma.project.fifam.repos.UserRepo;
+import ukma.project.fifam.utils.JwtUtil;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+public class CategoryController {
+    @Autowired
+    private CategoryRepo catRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @GetMapping(value = "/categories")
+    public ResponseEntity<?> getCategories(@RequestHeader("Authorization") String authHeader){
+        Optional<User> currUser = getCurrentUser(authHeader);
+        if (!currUser.isPresent()) return ResponseEntity.badRequest().body("User not found");
+        return ResponseEntity.ok(currUser.get().getCategories());
+    }
+
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<?> getCategoryById(@RequestHeader("Authorization") String authHeader,
+                                          @PathVariable String id){
+        Optional<User> currUser = getCurrentUser(authHeader);
+        if (!currUser.isPresent()) return ResponseEntity.badRequest().body("User not found");
+        return findCategoryById(currUser.get(), id);
+    }
+
+    @PostMapping("/categories")
+    public ResponseEntity<?> createCategory(@RequestHeader("Authorization") String authHeader,
+                                            @RequestBody Map<String, String> body){
+        Optional<User> currUser = getCurrentUser(authHeader);
+        if (!currUser.isPresent()) return ResponseEntity.badRequest().body("User not found");
+        String name = body.get("name");
+        String budget = body.get("budget");
+        Frequency freq = Frequency.valueOf(body.get("frequency"));
+        User user = currUser.get();
+
+        return ResponseEntity.ok(catRepo.save(new Category(name, budget, freq, user)));
+    }
+
+    @PutMapping("/categories/{id}")
+    public ResponseEntity<?> updateCategory(@RequestHeader("Authorization") String authHeader,
+                                            @PathVariable String id,
+                                            @RequestBody Map<String, String> body){
+        Optional<User> currUser = getCurrentUser(authHeader);
+        if (!currUser.isPresent()) return ResponseEntity.badRequest().body("User not found");
+        ResponseEntity resp = findCategoryById(currUser.get(), id);
+        if (!resp.getStatusCode().equals(HttpStatus.OK)) return resp;
+        Category category = (Category) resp.getBody();
+        assert category != null;
+
+        String name = body.get("name");
+        if (name != null) category.setName(name);
+        String budget = body.get("budget");
+        if (budget != null) category.setBudget(budget);
+        String currExpenses = body.get("currentExpenses");
+        if (currExpenses != null) category.setCurrentExpenses(currExpenses);
+        String freq = body.get("frequency");
+        if (freq != null) category.setFreq(Frequency.valueOf(freq));
+
+        return ResponseEntity.ok(catRepo.save(category));
+    }
+
+    @DeleteMapping("/categories/{id}")
+    public ResponseEntity<?> deleteCategory(@RequestHeader("Authorization") String authHeader,
+                                            @PathVariable String id){
+        Optional<User> currUser = getCurrentUser(authHeader);
+        if (!currUser.isPresent()) return ResponseEntity.badRequest().body("User not found");
+        ResponseEntity resp = findCategoryById(currUser.get(), id);
+        if (!resp.getStatusCode().equals(HttpStatus.OK)) return resp;
+
+        Category category = (Category) resp.getBody();
+        catRepo.delete(category);
+        return ResponseEntity.noContent().build();
+    }
+
+    private Optional<User> getCurrentUser(String header){
+        String userId = jwtUtil.getUserIdFromToken(header);
+        return userRepo.findById(Long.parseLong(userId));
+    }
+
+    private ResponseEntity<?> findCategoryById(User currUser, String id){
+        Long catId = Long.parseLong(id);
+        Optional<Category> category = catRepo.findById(catId);
+        if (!category.isPresent()) return ResponseEntity.badRequest().body("Category not found");
+        Category categoryFound = category.get();
+        if (!categoryFound.getUser().equals(currUser)) {
+            return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+        }
+        return ResponseEntity.ok(categoryFound);
+    }
+
+}
--- src/main/java/ukma/project/fifam/models/Category.java
+++ src/main/java/ukma/project/fifam/models/Category.java
@@ -1,5 +1,6 @@
 package ukma.project.fifam.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import ukma.project.fifam.Frequency;
 
 import javax.persistence.*;
@@ -18,13 +19,26 @@ public class Category {
     @Column(name = "budget", precision = 10, scale = 2, nullable = false)
     private String budget;
 
+    @Column(name = "currentExpenses", columnDefinition = "Decimal(10,2) default '0.00'", nullable = false)
+    private String currentExpenses;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "freq", nullable = false)
     private Frequency freq;
 
-    @ManyToOne
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "userId", nullable = false)
     private User user;
+
+    public Category(){}
+
+    public Category(String name, String budget, Frequency freq, User user) {
+        this.name = name;
+        this.budget = budget;
+        this.freq = freq;
+        this.user = user;
+    }
 
     public long getId() {
         return id;
@@ -50,6 +64,14 @@ public class Category {
         this.budget = budget;
     }
 
+    public String getCurrentExpenses() {
+        return currentExpenses;
+    }
+
+    public void setCurrentExpenses(String currentExpenses) {
+        this.currentExpenses = currentExpenses;
+    }
+
     public Frequency getFreq() {
         return freq;
     }
@@ -72,8 +94,8 @@ public class Category {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", budget='" + budget + '\'' +
+                ", currentExpenses='" + currentExpenses + '\'' +
                 ", freq=" + freq +
-                ", user=" + user +
                 '}';
     }
 
@@ -84,12 +106,13 @@ public class Category {
         Category category = (Category) o;
         return id == category.id &&
                 name.equals(category.name) &&
+                currentExpenses.equals(category.currentExpenses) &&
                 budget.equals(category.budget) &&
                 freq == category.freq;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, budget, freq);
+        return Objects.hash(id, name, budget, currentExpenses, freq);
     }
 }
